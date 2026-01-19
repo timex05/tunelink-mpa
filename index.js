0bbb5946783f56e325e3c626f7b408fc1b78a0b9
Timo Müller <timex385@gmail.com>
2026-01-19 10:05:58 +0100

    stuff

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// optional: default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
