3d3d9dbb27d60eb19f0c7ae4a1b5d59e9eccccc8
Timo Müller <timex385@gmail.com>
2026-01-19 10:11:46 +0100

    env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
+require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // serve static files
 app.use(express.static(path.join(__dirname, "public")));
